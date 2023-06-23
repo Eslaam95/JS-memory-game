210bb1a67990e557b5db56f1cf8d148a4ff4f307
Eslaam <eslaam95m@gmail.com>
2023-06-23 23:57:14 +0200

    Created the markup and main win/lose  functions

---
+++ script.js
@@ -0,0 +1,64 @@
+let cards = document.querySelectorAll('.card');
+let isFilppedCard = false, 
+firstCard, secondCard, stopFlipping= false;
+const successAud = document.getElementById('success');
+
+cards.forEach((card)=>{
+    card.addEventListener('click', flipCard);
+})
+
+
+
+
+function flipCard(){
+    if(stopFlipping === true )  return;
+    if(this === firstCard )  return;
+    this.classList.add('flipped');
+    /*first cards?*/
+    if(!isFilppedCard){
+        firstCard = this;
+        isFilppedCard = true;
+    }else{
+        /*check match*/
+        isFilppedCard=false;
+        secondCard=this;
+        checkMatching();
+    }
+}
+
+
+function checkMatching(){
+
+    if(secondCard.dataset.match === firstCard.dataset.match){
+        console.log("match");
+        firstCard.removeEventListener('click', flipCard);
+        secondCard.removeEventListener('click', flipCard);
+        successAud.play();
+        resetBoard();
+    } else{
+       
+        stopFlipping=true;
+       setTimeout(()=>{
+            firstCard.classList.remove("flipped");
+            secondCard.classList.remove("flipped");
+            
+            resetBoard();
+        },700);
+       
+    }
+}
+
+
+function resetBoard(){
+    [isFilppedCard, stopFlipping] = [false,false]
+    firstCard = null;
+    secondCard = null;
+}
+
+
+(function suffleCard(){
+    cards.forEach((card) =>{
+        let randOrder = Math.floor(Math.random()*12);
+        card.style.order = randOrder;
+    })
+})();
