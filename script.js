50039190f4b25c3769967c211b6cf20b7682aa41
eslaam95 <eslaam95m@gmail.com>
2023-08-11 18:50:12 +0200

    code optimization

--- script.js
+++ script.js
@@ -7,23 +7,12 @@ let isFilppedCard = false,
   secs = 0,
   totalMoves = 0,
   wrong = 0,
-  cards;
+  cards,
+  level = 1,
+  cardsNum,
+  time;
 
-const successAud = document.getElementById("success");
-const successAudFull = document.getElementById("full_success");
-const minsElm = document.getElementById("min");
-const secsElm = document.getElementById("sec");
-const wrongElm = document.getElementById("wrong");
-const totalElm = document.getElementById("total");
-const topScoreElm = document.getElementById("top");
-const celebElm = document.getElementById("celebrate");
-const tryAgainElm = document.getElementById("try-again");
-const tryAgainBox = document.getElementById("again");
-
-const overlayElm = document.getElementById("overlay");
-let topScore = Number(localStorage.getItem("score"));
-let currLevelElm = document.getElementById("curr");
-topScoreElm.innerHTML = topScore;
+/*image array*/
 const imgsArr = [
   "assets/B.svg",
   "assets/C.svg",
@@ -42,18 +31,36 @@ const imgsArr = [
   "assets/C.svg",
   "assets/B.svg",
 ];
+/*DOM selectors*/
+const successAud = document.getElementById("success");
+const successAudFull = document.getElementById("full_success");
+const minsElm = document.getElementById("min");
+const secsElm = document.getElementById("sec");
+const wrongElm = document.getElementById("wrong");
+const totalElm = document.getElementById("total");
+const topScoreElm = document.getElementById("top");
+const celebElm = document.getElementById("celebrate");
+const tryAgainElm = document.getElementById("try-again");
+const tryAgainBox = document.getElementById("again");
+const overlayElm = document.getElementById("overlay");
+let currLevelElm = document.getElementById("curr");
 const cont = document.getElementById("cards-cont");
-let level = 1,
-  cardsNum;
-overlayElm.style.display = "none";
-celebElm.style.display = "none";
-tryAgainBox.style.display = "none";
+/*get top score*/
+let topScore = Number(localStorage.getItem("score"));
+topScoreElm.innerHTML = topScore;
+resetCelebrationScreen();
+handleLevel();
+
+/*Every level settings*/
 function handleLevel() {
-  overlayElm.style.display = "none";
-  celebElm.style.display = "none";
-  tryAgainBox.style.display = "none";
+  /*pre-level settings*/
+  resetCelebrationScreen();
   currLevelElm.innerHTML = level;
+  cont.innerHTML = "";
+  matchFull = 0;
+  /*determine lvel and cards number*/
   if (level == 1) {
+    setTimer();
     cardsNum = 6;
     cont.classList.remove("level-3");
     cont.classList.add("level-1");
@@ -68,8 +75,7 @@ function handleLevel() {
     cont.classList.remove("level-2");
     cont.classList.add("level-3");
   }
-  cont.innerHTML = "";
-  matchFull = 0;
+  /*Create and add cards*/
   for (let i = 0; i < cardsNum / 2; i++) {
     for (let j = 0; j < 2; j++) {
       let x = document.createElement("div");
@@ -82,31 +88,44 @@ function handleLevel() {
     }
   }
   cards = document.querySelectorAll(".card");
-
   cards.forEach((card) => {
     card.addEventListener("click", flipCard);
   });
   suffleCard();
 }
-handleLevel();
 
-let time = setInterval(() => {
-  if (secs < 59) {
-    ++secs;
-  } else if (secs == 59) {
-    secs = 0;
-    ++mins;
-  }
-  secs < 10 ? (secsElm.innerHTML = "0" + secs) : (secsElm.innerHTML = secs);
-  mins < 10 ? (minsElm.innerHTML = "0" + mins) : (minsElm.innerHTML = mins);
-}, 1000);
+/*shuffer cards based on order*/
+function suffleCard() {
+  cards.forEach((card) => {
+    let randOrder = Math.floor(Math.random() * cardsNum);
+    card.style.order = randOrder;
+  });
+}
 
+/*timer count function*/
+function setTimer() {
+  time = setInterval(() => {
+    if (secs < 59) {
+      ++secs;
+    } else if (secs == 59) {
+      secs = 0;
+      ++mins;
+    }
+    secs < 10 ? (secsElm.innerHTML = "0" + secs) : (secsElm.innerHTML = secs);
+    mins < 10 ? (minsElm.innerHTML = "0" + mins) : (minsElm.innerHTML = mins);
+  }, 1000);
+}
+function clearTimer() {
+  clearInterval(time);
+  secsElm.innerHTML = "00";
+  minsElm.innerHTML = "00";
+}
+/*Click 3d flip couple cards*/
 function flipCard() {
   if (stopFlipping === true) return;
   if (this === firstCard) return;
-  console.log("clicked first", firstCard);
   this.classList.add("flipped");
-  /*first cards?*/
+  /*first card?*/
   if (!isFilppedCard) {
     firstCard = this;
     isFilppedCard = true;
@@ -114,25 +133,21 @@ function flipCard() {
     /*check match*/
     isFilppedCard = false;
     secondCard = this;
-    console.log("clicked second", secondCard);
-
     checkMatching();
   }
 }
 
 function checkMatching() {
+  /*increase moves*/
   totalMoves++;
-
   totalElm.innerHTML = totalMoves;
   if (secondCard.dataset.match === firstCard.dataset.match) {
-    console.log("match");
     firstCard.removeEventListener("click", flipCard);
     secondCard.removeEventListener("click", flipCard);
-    ++matchFull;
 
+    ++matchFull;
     if (matchFull === cards.length / 2) {
       successAudFull.play();
-
       level < 3 ? nextLevel() : celebrate();
     } else {
       successAud.play();
@@ -149,19 +164,13 @@ function checkMatching() {
     }, 700);
   }
 }
-
+/*reset every time clicking 2 cards*/
 function resetBoard() {
   [isFilppedCard, stopFlipping] = [false, false];
   firstCard = null;
   secondCard = null;
 }
-
-function suffleCard() {
-  cards.forEach((card) => {
-    let randOrder = Math.floor(Math.random() * cardsNum);
-    card.style.order = randOrder;
-  });
-}
+/*reset game after clicking try-again button*/
 tryAgainElm.addEventListener("click", () => {
   level = 1;
   totalElm.innerHTML = "0";
@@ -169,6 +178,7 @@ tryAgainElm.addEventListener("click", () => {
   clearInterval(time);
   handleLevel();
 });
+/*celebration screen*/
 function celebrate() {
   topScore < 10 || totalMoves < topScore ? updateScore() : "";
   totalMoves = 0;
@@ -176,7 +186,7 @@ function celebrate() {
   overlayElm.style.display = "block";
   celebElm.style.display = "block";
   tryAgainBox.style.display = "flex";
-  clearInterval(time);
+  clearTimer();
 }
 function nextLevel() {
   level++;
@@ -186,4 +196,9 @@ function updateScore() {
   localStorage.setItem("score", totalMoves);
   topScore = Number(localStorage.getItem("score"));
   topScoreElm.innerHTML = topScore;
+}
+function resetCelebrationScreen() {
+  overlayElm.style.display = "none";
+  celebElm.style.display = "none";
+  tryAgainBox.style.display = "none";
 }
